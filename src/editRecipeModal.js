17ef9bbfa07ec2c88909f9a0d00fc356c79b5156
Meem0 <themeem0gmail.com>
2017-12-13 22:15:43 -0500

    Edit recipe modal with ingredients, reads from recipe from context menu

--- src/editRecipeModal.js
+++ src/editRecipeModal.js
@@ -1,13 +1,44 @@
 ngapp.controller('editRecipeModalController', function($scope) {
-    // initialize scope variables
-    $scope.message = `ayy lets make some recipes for ${xelib.FullName($scope.modalOptions.handle)}`;
-    $scope.ref_handle = $scope.modalOptions.handle;
-    $scope.ref_value = '';
-    $scope.ref_signature = xelib.Signature($scope.ref_handle);
-    $scope.ref_signatures = ['ARMO', 'WEAP'];
 
-    // scope functions
+    $scope.addIngredient = function() {
+        $scope.recipeModel.ingredients.push({item: '', count: 0});
+    }
+
+    $scope.removeIngredient = function(ingredient) {
+        let index = $scope.recipeModel.ingredients.indexOf(ingredient);
+        if (index >= 0) {
+            $scope.recipeModel.ingredients.splice(index, 1);
+        }
+    }
+
     $scope.closeModal = function() {
         $scope.$emit('closeModal');
     };
+
+    $scope.recipeModel = {
+        editorId: '',
+        createdObject: '',
+        createdObjectCount: 1,
+        ingredients: []
+    };
+
+    $scope.createdObjectSignatures = ['ARMO', 'WEAP'];
+    $scope.createdObjectSignature = $scope.createdObjectSignatures[0];
+
+    $scope.ingredientSignatures = ['ARMO', 'WEAP', 'INGR', 'MISC'];
+
+    let handle = $scope.modalOptions.handle;
+    if (xelib.Signature(handle) === 'COBJ') {
+        $scope.recipeModel.editorId = xelib.EditorID(handle);
+        $scope.recipeModel.createdObject = xelib.GetValue(xelib.GetElement(handle, 'CNAM - Created Object'));
+        $scope.recipeModel.createdObjectCount = xelib.GetUIntValue(xelib.GetElement(handle, 'NAM1 - Created Object Count'));
+
+        let itemsElement = xelib.GetElement(handle, 'Items');
+        $scope.recipeModel.ingredients = xelib.GetElements(itemsElement).map(itemHandle => {
+            return {
+                item: xelib.GetValue(xelib.GetElement(itemHandle, 'CNTO - Item\\Item')),
+                count: xelib.GetUIntValue(xelib.GetElement(itemHandle, 'CNTO - Item\\Count'))
+            }
+        });
+    }
 });
